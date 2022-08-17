bcd317b60d625cfab10468024f8ebeffe657dd84
Lucas Frazier <44044601+LucasFrazier@users.noreply.github.com>
2022-08-17 13:42:32 -0400

    Update Outside.js

--- src/components/Outside/Outside.js
+++ src/components/Outside/Outside.js
@@ -4,8 +4,8 @@ import "./Outside.css";
 
 export const Outside = ({ isDark, toggleDarkMode }) => {
   return (
-    <div className={isDark ? "outside outside--isDark" : "outside"}>
-      {isDark ? <h1>It's Dark</h1> : <h1>It's Light</h1>}
+    <div className={`outside ${isDark ? "outside--isDark" : ""}`}>
+      <h1>It's {isDark ? "Dark" : "Light"}</h1>
       <Middle isDark={isDark} toggleDarkMode={toggleDarkMode} />
     </div>
   );
